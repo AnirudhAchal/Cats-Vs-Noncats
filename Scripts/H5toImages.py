c96f0de69f2316b0b6bd840e150b3145ec9959b5
Anirudh Achal <anirudh.achal@gmail.com>
2020-11-06 15:27:26 +0530

    Scipts Updated

--- Scripts/H5toImages.py
+++ Scripts/H5toImages.py
@@ -1,3 +1,4 @@
+import os
 import h5py
 from PIL import Image
 import numpy as np
@@ -6,6 +7,16 @@ import numpy as np
 train_dataset = h5py.File('../Datasets/TrainCatsVsNoncats.h5', "r")
 train_dataset_X = np.array(train_dataset["train_set_x"][:])
 train_dataset_y = np.array(train_dataset["train_set_y"][:])
+
+
+# Make Directories
+os.mkdir(os.path.join('../Datasets/', 'TrainImages'))
+os.mkdir(os.path.join('../Datasets/', 'TestImages'))
+os.mkdir(os.path.join('../Datasets/TrainImages/', 'Cats'))
+os.mkdir(os.path.join('../Datasets/TrainImages/', 'Noncats'))
+os.mkdir(os.path.join('../Datasets/TestImages/', 'Cats'))
+os.mkdir(os.path.join('../Datasets/TestImages/', 'Noncats'))
+
 
 cat_count = 1
 noncat_count = 1
@@ -24,6 +35,7 @@ for image, label in zip(train_dataset_X, train_dataset_y):
 test_dataset = h5py.File('../Datasets/TestCatsVsNoncats.h5', "r")
 test_dataset_X = np.array(test_dataset["test_set_x"][:])
 test_dataset_y = np.array(test_dataset["test_set_y"][:])
+
 
 cat_count = 1
 noncat_count = 1
